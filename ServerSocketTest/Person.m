81b808bddf4cd8d8243dda2c9c8d188af079f307
myxc <70953073@qq.com>
2018-06-20 21:46:17 +0800

    git test 6

--- ServerSocketTest/Person.m
+++ ServerSocketTest/Person.m
@@ -11,14 +11,6 @@
 @implementation Person
 
 
-
-//因为各个运营商开头号码不同，这里只匹配11位是比较好的方法
-- (BOOL)isValidPhoneNumber {
-    NSString *regex = @"^[0-9]{11}";
-    return [self isValidateByRegex:regex];
-}
-
-
 //或者精确点，但需要根据运营商更新
 - (BOOL)wl_isMobileNumberClassification {
     /**
@@ -64,13 +56,48 @@
     }
 }
 
+- (BOOL)isValidUrlsfsfs{
+    NSString * regex = @"\\\\b((ftp|http|https?):\\\\/\\\\/[-\\\\w]+(\\\\.\\\\w[-\\\\w]*)+|(?i:[a-z0-9](?:[-a-z0-9]*[a-z0-9])?\\\\.)+(?-i:com\\\\b|edu\\\\b|biz\\\\b|gov\\\\b|in(?:t|fo)\\\\b|mil\\\\b|net\\\\b|org\\\\b|[a-z][a-z]\\\\b))(:\\\\d+)?(/[^.!,?;\\";
+    return [self isValidateByRegex:regex];
+}
+
+/**
+ @brief     是否符合最小长度、最长长度，是否包含中文,数字，字母，其他字符，首字母是否可以为数字
+ @param     minLenth 账号最小长度
+ @param     maxLenth 账号最长长度
+ @param     containChinese 是否包含中文
+ @param     containDigtal   包含数字
+ @param     containLetter   包含字母
+ @param     containOtherCharacter   其他字符
+ @param     firstCannotBeDigtal 首字母不能为数字
+ @return    正则验证成功返回YES, 否则返回NO
+ */
+- (BOOL)isValidWithMinLenth:(NSInteger)minLenth
+                   maxLenth:(NSInteger)maxLenth
+             containChinese:(BOOL)containChinese
+              containDigtal:(BOOL)containDigtal
+              containLetter:(BOOL)containLetter
+      containOtherCharacter:(NSString *)containOtherCharacter
+        firstCannotBeDigtal:(BOOL)firstCannotBeDigtal;
+{
+    NSString *hanzi = containChinese ? @"\\u4e00-\\u9fa5" : @"";
+    NSString *first = firstCannotBeDigtal ? @"^[a-zA-Z_]" : @"";
+    NSString *lengthRegex = [NSString stringWithFormat:@"(?=^.{%@,%@}$)", @(minLenth), @(maxLenth)];
+    NSString *digtalRegex = containDigtal ? @"(?=(.*\\\\d.*){1})" : @"";
+    NSString *letterRegex = containLetter ? @"(?=(.*[a-zA-Z].*){1})" : @"";
+    NSString *characterRegex = [NSString stringWithFormat:@"(?:%@[%@A-Za-z0-9%@]+)", first, hanzi, containOtherCharacter ? containOtherCharacter : @""];
+    NSString *regex = [NSString stringWithFormat:@"%@%@%@%@", lengthRegex, digtalRegex, letterRegex, characterRegex];
+    return [self isValidateByRegex:regex];
+}
+
+
 - (BOOL)isEmailAddress {
     NSString *emailRegex = @"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,4}";
     return [self isValidateByRegex:emailRegex];
 }
 
 - (BOOL)isValidUrl {
-    NSString * regex = @"\\\\b((ftp|http|https?):\\\\/\\\\/[-\\\\w]+(\\\\.\\\\w[-\\\\w]*)+|(?i:[a-z0-9](?:[-a-z0-9]*[a-z0-9])?\\\\.)+(?-i:com\\\\b|edu\\\\b|biz\\\\b|gov\\\\b|in(?:t|fo)\\\\b|mil\\\\b|net\\\\b|org\\\\b|[a-z][a-z]\\\\b))(:\\\\d+)?(/[^.!,?;\\";
+    NSString * regex = @"\\\\b((ftp|http|https?):\\\\/\\\\/[-\\\\w]+(\\\\.\\\\w[-\\\\w]*)+|(?i:[a-z0-9](?:[-a-z0-9]*[a-z0-9])?\\\\.)+(?-i:com\\\\b|edu\\\\b|biz\\\\b|gov\\\\b|in(?:t|fo)\\\\b|mil\\\\b|net\\\\b|org\\\\b|[a-z][a-z]\\\\b))(:\\\\d+)?(/[^.!,?;\\ionvrbnotrinoprenionribrijkngrgtgoirehopghpojp[rjgoprhj59j";
     return [self isValidateByRegex:regex];
 }
 
@@ -79,10 +106,6 @@
     return [self isValidateByRegex:regex2];
 }
 
-- (BOOL)validateCarType {
-    NSString *regex2 = @"^[\\u4E00-\\u9FFF]+$";
-    return [self isValidateByRegex:regex2];
-}
 - (BOOL)isValidPassword {
     //以字母开头，只能包含“字母”，“数字”，“下划线”，长度6~18
     NSString *regex = @"^([a-zA-Z]|[a-zA-Z0-9_]|[0-9]){6,18}$";
